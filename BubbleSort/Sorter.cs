748749d1db2e07143251a12e6ad48d00a6179382
a91n0d <a91n0d@gmail.com>
2022-01-12 09:11:22 +0300

    Add project files.

---
+++ BubbleSort/Sorter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+#pragma warning disable SA1611
+
+namespace BubbleSort
+{
+    public static class Sorter
+    {
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with bubble sort algorithm.
+        /// </summary>
+        public static void BubbleSort(this int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int i = array.Length; i > 0; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sorts an <paramref name="array"/> with recursive bubble sort algorithm.
+        /// </summary>
+        public static void RecursiveBubbleSort(this int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            array.RecursiveBubbleSort(array.Length);
+        }
+
+        public static void RecursiveBubbleSort(this int[] array, int n)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (n == 1 || n == 0)
+            {
+                return;
+            }
+
+            for (int j = 1; j < n; j++)
+            {
+                if (array[j] < array[j - 1])
+                {
+                    (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                }
+            }
+
+            RecursiveBubbleSort(array, n - 1);
+        }
+    }
+}
